b1327f707044dcb0a76c7e7e6f8aa6fb6182fedc
XiaoPan <2489964425@qq.com>
2023-07-01 12:32:38 +0800

    refactor(main.go): coco framework used

--- main.go
+++ main.go
@@ -8,11 +8,25 @@ import (
 
 func main() {
 	co := coco.New()
+	co.GET("/", func(c *coco.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello coco</h1>")
+	})
+
+	co.GET("/userInfo", func(c *coco.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
 	co.POST("/login", func(c *coco.Context) {
 		fmt.Println(c.Path)
 		c.JSON(http.StatusOK, coco.H{
 			"username": c.PostForm("username"),
 			"password": c.PostForm("password"),
+		})
+	})
+
+	co.GET("/assets/*filepath", func(c *coco.Context) {
+		c.JSON(http.StatusOK, coco.H{
+			"filepath": c.Param("filepath"),
 		})
 	})
 	_ = co.Run(":9999")
