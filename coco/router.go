658a012bc7313037714d05d8113e6bb058162f93
XiaoPan <2489964425@qq.com>
2023-07-01 09:56:03 +0800

    feat(router.go): new router trie & get router trie node

--- coco/router.go
+++ coco/router.go
@@ -3,17 +3,12 @@ package coco
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
-}
-
-// addRouter 将路由添加到路由表里
-func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "_" + pattern
-	log.Printf("Route %4s - %s", method, pattern)
-	r.handlers[key] = handler // 注册路由表
 }
 
 // Handle 执行对应路由下的函数
@@ -26,7 +21,71 @@ func (r *router) Handle(c *Context) {
 	}
 }
 
-// GenerateRouter 生成一个路由表
-func GenerateRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+// newRouter 生成一个路由表
+func newRouter() *router {
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+/*
+/ => []
+/login/a => [login a]
+/login/:router => [login :router]
+/login/* => [login *]
+/login/*router/a/b => [login *router]
+*/
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			// 遇到 * 后面就不用管了
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+// addRouter 将路由添加到路由表里，构建路由树
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+	key := method + "_" + pattern
+	if _, ok := r.roots[method]; !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	log.Printf("Route %4s - %s", method, pattern)
+	r.handlers[key] = handler // 注册路由表
+}
+
+// getRouter 这里就是用户请求进来的时候，遍历树
+func (r *router) getRouter(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	n := root.search(searchParts, 0)
+	if n != nil {
+		// 这个节点下的匹配路径
+		parts := parsePattern(n.pattern)
+		for i, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[i]
+			}
+			// *xxx
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[i:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
 }
